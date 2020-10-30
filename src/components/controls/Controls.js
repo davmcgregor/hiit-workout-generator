0e8525391d2cb55b5cd4a213834b2da2e58a7d2c
David McGregor <davidjohnmcgregor@gmail.com>
2020-10-30 14:34:43 +0000

    Add button svg icons

--- src/components/controls/Controls.js
+++ src/components/controls/Controls.js
@@ -5,7 +5,7 @@ import { getWorkout, startCountdown } from '../../actions/workout';
 import PropTypes from 'prop-types';
 
 const buttonStyles =
-  'w-64 uppercase tracking-wider text-white bg-blue-800 hover:bg-blue-700 focus:outline-none focus:border-blue-900 focus:shadow-outline-blue active:bg-blue-900 transition ease-in-out duration-150 text-2xl py-5 px-16 rounded-lg mx-4 mb-8';
+  'inline-flex items-center justify-center w-64 uppercase tracking-wider text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150 text-2xl py-5 px-10 rounded-lg mx-4 mb-8';
 
 const Controls = ({ startCountdown, getWorkout, workout: { currentView } }) => {
   return (
@@ -16,11 +16,45 @@ const Controls = ({ startCountdown, getWorkout, workout: { currentView } }) => {
           onClick={() => startCountdown()}
           class={buttonStyles}
         >
+          <svg
+            class='w-12 h-12 -ml-0.5 mr-2'
+            fill='none'
+            stroke='currentColor'
+            viewBox='0 0 24 24'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              stroke-width='2'
+              d='M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z'
+            ></path>
+            <path
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              stroke-width='2'
+              d='M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
+            ></path>
+          </svg>
           Begin
         </button>
       </span>
       <span class='inline-flex rounded-md'>
         <button type='button' onClick={() => getWorkout()} class={buttonStyles}>
+          <svg
+            class='w-12 h-12 -ml-0.5 mr-2'
+            fill='none'
+            stroke='currentColor'
+            viewBox='0 0 24 24'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              stroke-width='2'
+              d='M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
+            ></path>
+          </svg>
           Random
         </button>
       </span>
